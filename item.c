461648be51884e1841e4b86113b9e486f7c2d4de
SDisanza <salvatore.disanza909@gmail.com>
2025-06-10 20:18:39 +0200

    Update item.c
    Semplificazione codice e aggiunta eccezione

--- item.c
+++ item.c
@@ -1,33 +1,28 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"studio.h"
-#include"item.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "studio.h"
+#include "item.h"
 #define NCHAR 100
 
-Item inputItem()
-{
-    char *pointerInput;
-
-    pointerInput=malloc(NCHAR * sizeof(char));
-    scanf("%s", pointerInput);
-
-    return pointerInput;
+Item inputItem() {
+    char *input = malloc(NCHAR * sizeof(char));
+    if (input == NULL) {
+        fprintf(stderr, "Errore di allocazione memoria\n");
+        exit(EXIT_FAILURE);
+    }
+    scanf("%99s", input); // Limita la lunghezza dell'input
+    return input;
 }
 
-void outputItem(Item item)
-{
-    char *pointerOuput;
-
-    pointerOuput=item;
-    printf("%s ", pointerOuput);
+void outputItem(Item item) {
+    printf("%s", (char*)item);
 }
 
-int cmpItem(Item itemA, Item itemB)
-{
-    char *pointerA, *pointerB;
+int cmpItem(Item itemA, Item itemB) {
+    return strcmp((char*)itemA, (char*)itemB);
+}
 
-    pointerA=itemA;
-    pointerB=itemB;
-
-    return strcmp(pointerA, pointerB);
+void freeItem(Item item) {
+    free(item);
 }
